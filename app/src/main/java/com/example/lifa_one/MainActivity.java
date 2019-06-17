68b3d1a9e6b1813e16cfe1d11a1c7f0abddfc810
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-06-17 07:28:41 +0300

    adds a form activity and sets a click listener on the proceed button in order to navigate to the form activity page

--- app/src/main/java/com/example/lifa_one/MainActivity.java
+++ app/src/main/java/com/example/lifa_one/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.lifa_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mMainTitle;
+    private Button mProceed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMainTitle = (TextView) findViewById(R.id.maintitle);
+        mProceed = (Button) findViewById(R.id.buttonProceed);
+
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Amadeus.ttf");
         mMainTitle.setTypeface(font);
 
+
+        mProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, formActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
