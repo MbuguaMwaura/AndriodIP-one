72ae2a81a21c7859153158c8cf5fc41cdc9f9c73
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-06-17 10:05:56 +0300

    adds the content and styling to the welcome page

--- app/src/main/java/com/example/lifa_one/WelcomeActivity.java
+++ app/src/main/java/com/example/lifa_one/WelcomeActivity.java
@@ -2,13 +2,33 @@ package com.example.lifa_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.TestLooperManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private TextView mWelcome;
+    private Button mEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mWelcome = (TextView) findViewById(R.id.welcome);
+        mEvents = (Button) findViewById(R.id.events);
+
+        Intent intent = getIntent();
+        String firstName = intent.getStringExtra("firstName");
+        String lastName = intent.getStringExtra("lastName");
+        String email = intent.getStringExtra("email");
+
+
+
+        mWelcome.setText("Hey there " + firstName + ", welcome to Life.Is.Freaking.Awesome. Click the button below and" +
+                " explore the amazing events you MUST attend!");
     }
 }
