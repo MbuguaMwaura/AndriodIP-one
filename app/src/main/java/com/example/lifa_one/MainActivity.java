31744ae85389ce185dd1700410f0fc247ffb3dae
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-06-17 07:21:33 +0300

    adds a font to the main title using the set typeface method

--- app/src/main/java/com/example/lifa_one/MainActivity.java
+++ app/src/main/java/com/example/lifa_one/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.lifa_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mMainTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainTitle = (TextView) findViewById(R.id.maintitle);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Amadeus.ttf");
+        mMainTitle.setTypeface(font);
+
     }
 }
